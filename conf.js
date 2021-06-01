35c00e34833b55559f03b387157b9b9b30b7180e
Pixcat <30306785+pixcati@users.noreply.github.com>
2021-06-01 17:44:09 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,33 +2,19 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '118',
+  ver: '312',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 79
+    ver: 60
   },
 
   // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
+ 
     'mysite': {
-      label: '当前站点',
+      label: 'local',
       lines: {
         [location.host]: 1,
       }
@@ -38,7 +24,15 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        'node-cfworker-2.etherdream.com': 1,
+        // 收费版（高权重）
+        'node-cfworker.etherdream.com': 4,
+
+        // 免费版（低权重，分摊一些成本）
+        // 每个账号每天 10 万次免费请求，但有频率限制
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -47,7 +41,7 @@ jsproxy_config({
    * 默认节点
    */
   // node_default: 'mysite',
-  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -58,10 +52,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/pixcati/JSproxy_php@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+//   assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
@@ -72,7 +66,7 @@ jsproxy_config({
   /**
    * 自定义注入页面的 HTML
    */
-  inject_html: '<!-- custom html -->',
+  inject_html: ',<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/sentsin/layui@master/dist/css/layui.css" /><div class="layer-footer" style="z-index: 10; position: fixed; text-align: right; margin-left: -40px; bottom: 50px; width:100%;"><a href="https://js.svvme.com/" target="_blank" style="background-color: #fb7299;" type="button" class="layui-btn layui-btn-nomal layui-btn-radius"><i class="layui-icon" style="font-size: 25px;">&#xe615;</i>返回</a></div>',
 
   /**
    * URL 自定义处理（设计中）
@@ -85,7 +79,7 @@ jsproxy_config({
       redir: 'https://php.net/'
     },
     'http://haha.com/': {
-      content: 'Hello World'
+      content: '<h1>Hello</h1>'
     },
   }
 })
